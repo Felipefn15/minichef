12d469a3da4aef580481feba94b662a5cacd94fd
Felipefn15 <felipefrancanogueira@gmail.com>
2023-12-26 17:05:07 -0300

    Update RecipeCard component with quantity of a portion

--- components/RecipeCard.tsx
+++ components/RecipeCard.tsx
@@ -1,4 +1,4 @@
-import { Clock } from "@tamagui/lucide-icons";
+import { Clock, SmilePlus } from "@tamagui/lucide-icons";
 import { Button, H3, H5, Stack, Text, XStack } from "tamagui";
 
 interface RecipeCardInterface {
@@ -6,6 +6,7 @@ interface RecipeCardInterface {
     name: string;
     timeToPrepare: number;
     difficulty: number;
+    quantityOfAPortion: number;
     ingredients: {
       name: string;
     }[];
@@ -37,12 +38,12 @@ export default function RecipeCard({ recipe }: RecipeCardInterface) {
           <Text>{recipe.timeToPrepare} minutes</Text>
         </XStack>
       </XStack>
-      <XStack
+      <Stack
         ai="center"
-        jc="space-between"
         w="80%"
+        gap={5}
       >
-        <Stack ai="flex-start">
+        {/* <Stack ai="flex-start">
           <H5>Ingredient</H5>
           <Stack
             w="100%"
@@ -52,12 +53,17 @@ export default function RecipeCard({ recipe }: RecipeCardInterface) {
               return <Text key={index}>- {ingredient.name}</Text>;
             })}
           </Stack>
+        </Stack> */}
+        <Stack ai="center">
+          <SmilePlus />
+          <H5>Quantity of a portion</H5>
+          <Text>{recipe.quantityOfAPortion}</Text>
         </Stack>
         <Stack ai="center">
           <H5>Difficulty</H5>
           <Text>{recipe.difficulty}</Text>
         </Stack>
-      </XStack>
+      </Stack>
     </Stack>
   );
 }
